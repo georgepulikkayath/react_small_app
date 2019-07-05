f1931db56f581e8be6ae3ee1894409480e9f0d7a
gk <georgepulikkayath@gmail.com>
2019-07-05 14:20:16 -0400

    navbar added

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Navbar extends Component {
+   
+    render() { 
+        return ( <div>
+            <nav className="navbar navbar-light bg-light">
+  <a className="navbar-brand" href="#">Navbar</a>
+</nav>
+        </div>
+         );
+    }
+}
+ 
+export default Navbar ;
